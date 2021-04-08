a1f346b389794d65a710a1f7521a76db5e405a23
edgarops <33764377+edgarops@users.noreply.github.com>
2021-04-07 18:21:46 -0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -19,5 +19,6 @@ app.use(
 app.use(appRoutes);
 
 
+const port = process.env.PORT || 3000
 
-app.listen(process.env.PORT || 3333)
+app.listen(port, () => console.log('server started on port', port))
