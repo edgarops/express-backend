12f2d80ddade938359349690893f2451490ba28b
edgarops <33764377+edgarops@users.noreply.github.com>
2021-05-15 15:31:39 -0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -10,8 +10,8 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    'Access-Control-Allow-Origin': 'https://photogramops.herokuapp.com',
-    optionsSuccessStatus: 200
+    origin:true,credentials: true
+    //optionsSuccessStatus: 200
     //exposedHeaders: "X-Total-Count"
 })); //aqui se podria poner direccion origin: 'ip' o con un array
 //app.use(timeout('15s'));
