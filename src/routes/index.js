cf98945c3601a17cadaba433b7f33c1ca7bd887b
edgarops <33764377+edgarops@users.noreply.github.com>
2021-05-15 15:16:41 -0600

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,19 +3,19 @@ const {Router} = require('express');
 const appRoutes = Router();
 
 const authRoutes = require("./authRoutes");
-const userRoutes = require("./userRoutes");
+/*const userRoutes = require("./userRoutes");
 const photoRoutes = require("./photoRoutes");
 const likeRoutes = require("./likeRoutes");
 const commentRoutes = require("./commentRoutes");
 const followRoutes = require("./followRoutes");
-const feedRoutes = require("./feedRoutes");
+const feedRoutes = require("./feedRoutes");*/
 
-appRoutes.use("/auth", authRoutes);
+appRoutes.use("/auth", authRoutes);/*
 appRoutes.use("/users", userRoutes);
 appRoutes.use("/photos", photoRoutes);
 appRoutes.use("/likes", likeRoutes);
 appRoutes.use("/comments", commentRoutes);
 appRoutes.use("/follows", followRoutes);
-appRoutes.use("/feeds", feedRoutes);
+appRoutes.use("/feeds", feedRoutes);*/
 
 module.exports = appRoutes;
