fd28bd0054e7ea4bcde4c624f22bb248ac4f2d54
edgarops <33764377+edgarops@users.noreply.github.com>
2021-05-15 15:41:20 -0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,13 +9,25 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors({
+/*app.use(cors({
     //origin:true,credentials: true
     origin: 'https://photogramops.herokuapp.com',
     optionsSuccessStatus: 200
     //exposedHeaders: "X-Total-Count"
-})); //aqui se podria poner direccion origin: 'ip' o con un array
+})); //aqui se podria poner direccion origin: 'ip' o con un array*/
 //app.use(timeout('15s'));
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // handle OPTIONS method
+    if ('OPTIONS' == req.method) {
+        return res.sendStatus(200);
+    } else {
+        next();
+    }
+});
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(
