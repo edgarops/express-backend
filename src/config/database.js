ee4e9db27c0d01abaadf543cccb1ff74b82c6967
edgarops <33764377+edgarops@users.noreply.github.com>
2021-05-15 17:28:47 -0600

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,18 +2,14 @@ require("dotenv").config();
 
 module.exports={
     dialect:"postgres",
-    host: process.env.DB_HOST,
+    /*host: process.env.DB_HOST,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    //use_env_variable: process.env.DATABASE_URL,
+    database: process.env.DB_DATABASE,*/
+    use_env_variable: process.env.DATABASE_URL,
     /*define: {
         timestamps: true,
         underscored: true
-    },*/
-     dialectOptions: {
-        ssl: {
-            require: false,
-            rejectUnauthorized: false
-        }
+    }*/
+    
 }
