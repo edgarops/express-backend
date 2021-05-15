2800c51e393b2ca30c4539d03dc68a64eaf42c81
edgarops <33764377+edgarops@users.noreply.github.com>
2021-05-15 17:28:58 -0600

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -6,7 +6,7 @@ module.exports={
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,*/
-    use_env_variable: process.env.DATABASE_URL,
+    use_env_variable: process.env.DATABASE_URL
     /*define: {
         timestamps: true,
         underscored: true
